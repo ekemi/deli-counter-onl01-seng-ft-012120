6b1ce492f43e15019b22f0490cf8b36c2ba2dae9
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-24 18:08:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,13 +7,6 @@ def line(array)
     
        puts "The line is currently empty."
   
-  
-  #elsif array.length != 0
-  
-  
-      # array.each_with_index {| name,position| puts "The line is currently: #{position+1}. ##{name}"}
-      
-     # ar
   elsif array.length != count
   
       array.each do |name|
