c3692b320c1c7872e87c7f71e80fa4454fad8440
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-24 17:48:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+ 
+def line(array)
+  count = 1
+  
+  if array.length == 0 
+    
+       puts "The line is currently empty."
+  
+  
+  #elsif array.length != 0
+  
+  
+      # array.each_with_index {| name,position| puts "The line is currently: #{position+1}. ##{name}"}
+      
+     # ar
+  elsif array.length != count
+  
+      array.each do |name|
+        
+        puts "The line is currently: #{count}. #{name}"
+        
+        count +=1
+
+      end
+  end  
+  
+end 
+
+
+def take_a_number
+  
+end 
+
+def now_serving
+  
+      end
